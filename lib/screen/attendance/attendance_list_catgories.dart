d093f8e78217ab555a51f4c3cb05cde572a6860f
itsanvert <itsanvert@gmail.com>
2025-12-30 04:00:36 +0700

    fix: appbar in attendance list catgories

--- lib/screen/attendance/attendance_list_catgories.dart
+++ lib/screen/attendance/attendance_list_catgories.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sbku_app/data/dummy_attendance.dart';
+import 'package:sbku_app/model/attendance.dart';
+import 'package:sbku_app/screen/attendance/attendance_list_view_screen.dart';
+import 'package:sbku_app/widget/appbar_widget.dart';
 import 'package:sbku_app/widget/list_card_widget.dart';
 
 class AttendanceListCategoryScreen extends StatelessWidget {
@@ -8,22 +12,34 @@ class AttendanceListCategoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[50],
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: AppBar(
-          title: Text('Teacher Attendance Categories'),
-          centerTitle: true,
-        ),
+      appBar: AppBarWidget.simple(
+        title: 'បញ្ជីវត្តមាន',
+        actions: [
+          // IconButton(
+          //   onPressed: () {
+          //     // Share functionality
+          //   },
+          //   icon: const Icon(Icons.share, color: Colors.white),
+          // ),
+          // IconButton(
+          //   onPressed: () {
+          //     // Add functionality
+          //   },
+          //   icon: const Icon(Icons.add, color: Colors.white),
+          // ),
+        ],
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: ListCardList(
-            items: const [
+            items: [
               ListCardItem(
                 icon: Icons.check_circle_outline,
                 label: 'Present Students',
-                // screen: PresentStudentsScreen(),
+                screen: AttendanceItemWidget(
+                  attendance: dummyAttendances[0],
+                ),
               ),
               ListCardItem(
                 icon: Icons.cancel_outlined,

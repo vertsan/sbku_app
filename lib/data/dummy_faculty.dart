0bc922e3f898eaa432fddc62051a197f59daf283
Yun Yuna <adviserna0@gmail.com>
2026-01-04 13:40:03 +0700

    refactor add dammy data of faculty model

--- lib/data/dummy_faculty.dart
+++ lib/data/dummy_faculty.dart
@@ -4,4 +4,5 @@ final dummyFaculties = [
   FacultyModel(id: 'F01', facultyName: 'Engineering',code: 'test123'),
   FacultyModel(id: 'F02', facultyName: 'Business Administration',code: 'test123'),
   FacultyModel(id: 'F03', facultyName: 'Education',code: 'test123'),
+  FacultyModel(id: 'F04', facultyName: 'IT',code: 'test123'),
 ];
